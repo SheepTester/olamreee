b4b3f60e39a9591bb257f263dcb41e5f9ceedd3c
SheepTester <ussa.say@gmail.com>
2018-10-27 10:18:43 -0700

    intuitive WHEEL ZOOM :D :D

--- brain.js
+++ brain.js
@@ -8,7 +8,9 @@ const options = {
   snap: true
 };
 
-const board = {};
+const win = {};
+
+const camera = {x: 0, y: 0, scale: 1};
 
 let globalOffsetX = 0, globalOffsetY = 0;
 
@@ -18,7 +20,7 @@ class Card {
     this.parent = parent;
     this.data = data;
     this.x = 0, this.y = 0;
-    this.dragging = false;
+    this.dragData = null;
     const elem = document.createElement('div');
     this.elem = elem;
     elem.classList.add('card');
@@ -26,8 +28,7 @@ class Card {
     elem.addEventListener('touchstart', e => {
       if (!options.drag) return;
       const touch = e.changedTouches[0];
-      this.startDragging();
-      this.dragData = {initX: touch.pageX - this.x, initY: touch.pageY - this.y};
+      this.initDragData(touch.clientX, touch.clientY);
       parent.touchDrags[touch.identifier] = this;
       e.preventDefault();
       this.parent.createTouchListeners();
@@ -35,9 +36,8 @@ class Card {
     }, {passive: false});
     elem.addEventListener('mousedown', e => {
       if (!options.drag) return;
-      if (!this.dragging) { // make sure it hasn't been touched beforehand
-        this.startDragging();
-        this.dragData = {initX: e.pageX - this.x, initY: e.pageY - this.y};
+      if (this.dragData === null) { // make sure it hasn't been touched beforehand
+        this.initDragData(e.clientX, e.clientY);
         parent.mouseDrag = this;
         e.preventDefault();
         this.parent.createMouseListeners();
@@ -65,21 +65,43 @@ class Card {
   checkAutoScroll() {
     if (!this.dragging) return;
     let diffX = 0, diffY = 0;
-    if (this.x - board.scrollX < GRID_SIZE / 2) window.scrollBy(diffX = -AUTO_SCROLL_SPEED, 0);
-    else if (this.x - board.scrollX > board.width - GRID_SIZE * 1.5) window.scrollBy(diffX = AUTO_SCROLL_SPEED, 0);
-    if (this.y - board.scrollY < GRID_SIZE / 2) window.scrollBy(0, diffY = -AUTO_SCROLL_SPEED);
-    else if (this.y - board.scrollY > board.height - GRID_SIZE * 1.5) window.scrollBy(0, diffY = AUTO_SCROLL_SPEED);
+    if (this.x - win.scrollX < GRID_SIZE / 2) window.scrollBy(diffX = -AUTO_SCROLL_SPEED, 0);
+    else if (this.x - win.scrollX > win.width - GRID_SIZE * 1.5) window.scrollBy(diffX = AUTO_SCROLL_SPEED, 0);
+    if (this.y - win.scrollY < GRID_SIZE / 2) window.scrollBy(0, diffY = -AUTO_SCROLL_SPEED);
+    else if (this.y - win.scrollY > win.height - GRID_SIZE * 1.5) window.scrollBy(0, diffY = AUTO_SCROLL_SPEED);
     this.setPos(this.x + diffX, this.y + diffY);
   }
 
   startDragging() {
-    this.dragging = true;
     this.elem.classList.add('dragged');
   }
 
   stopDragging() {
-    this.dragging = false;
+    this.dragData = null;
     this.elem.classList.remove('dragged');
+  }
+
+  initDragData(mouseX, mouseY) {
+    const visualGridSize = GRID_SIZE * camera.scale;
+    this.dragData = {
+      dragging: false,
+      offsetX: (mouseX - this.x) / visualGridSize,
+      offsetY: (mouseY - this.y) / visualGridSize,
+      imitX: mouseX, initY: mouseY
+    };
+  }
+
+  setDragPos(mouseX, mouseY) {
+    const visualGridSize = GRID_SIZE * camera.scale;
+    if (!this.dragData.dragging) {
+      if (Math.abs(this.dragData.initX - mouseX) > 4 || Math.abs(this.dragData.initY - mouseY) > 4) {
+        this.dragData.dragging = true;
+        this.startDragging();
+      } else {
+        return;
+      }
+    }
+    this.setPos(mouseX - this.dragData.offsetX * visualGridSize, mouseY - this.dragData.offsetY * visualGridSize);
   }
 
 }
@@ -92,7 +114,7 @@ function init([elements]) {
     Object.values(e.changedTouches).forEach(touch => {
       const card = cardParent.touchDrags[touch.identifier];
       if (!card) return;
-      card.setPos(touch.pageX - card.dragData.initX, touch.pageY - card.dragData.initY);
+      card.setDragPos(touch.clientX, touch.clientY);
     });
     e.preventDefault();
   }
@@ -100,7 +122,7 @@ function init([elements]) {
     Object.values(e.changedTouches).forEach(touch => {
       const card = cardParent.touchDrags[touch.identifier];
       if (!card) return;
-      card.setPos(touch.pageX - card.dragData.initX, touch.pageY - card.dragData.initY);
+      card.setDragPos(touch.clientX, touch.clientY);
       card.stopDragging();
       card.snap();
       delete cardParent.touchDrags[touch.identifier];
@@ -115,13 +137,13 @@ function init([elements]) {
   function mouseMove(e) {
     const card = cardParent.mouseDrag;
     if (!card) return;
-    card.setPos(e.pageX - card.dragData.initX, e.pageY - card.dragData.initY);
+    card.setDragPos(e.clientX, e.clientY);
     e.preventDefault();
   }
   function mouseEnd(e) {
     const card = cardParent.mouseDrag;
     if (!card) return;
-    card.setPos(e.pageX - card.dragData.initX, e.pageY - card.dragData.initY);
+    card.setDragPos(e.clientX, e.clientY);
     card.stopDragging();
     card.snap();
     cardParent.mouseDrag = null;
@@ -160,13 +182,37 @@ function init([elements]) {
 
   document.body.style.backgroundImage = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${GRID_SIZE}' height='${GRID_SIZE}' fill='none' stroke='rgba(0,0,0,0.1)'%3E%3Cpath d='M0 ${GRID_SIZE}H${GRID_SIZE}V0'/%3E%3C/svg%3E")`;
 
-  function paint() {
-    board.height = window.innerHeight;
-    board.width = window.innerWidth;
-    board.scrollX = window.scrollX;
-    board.scrollY = window.scrollY;
+  window.addEventListener('wheel', e => {
+    if (e.ctrlKey) {
+      const change = Math.abs(e.deltaY / 1000) + 1;
+      const oldScale = camera.scale;
+      let xDiff = -camera.x * oldScale - e.clientX, yDiff = -camera.y * oldScale - e.clientY;
+      if (e.deltaY > 0) {
+        camera.scale /= change, xDiff /= change, yDiff /= change;
+      } else if (e.deltaY < 0) {
+        camera.scale *= change, xDiff *= change, yDiff *= change;
+      }
+      camera.x = -(e.clientX + xDiff) / camera.scale;
+      camera.y = -(e.clientY + yDiff) / camera.scale;
+      e.preventDefault();
+    } else {
+      camera.x += e.shiftKey ? e.deltaY : e.deltaX;
+      camera.y += e.shiftKey ? e.deltaX : e.deltaY;
+      if (e.deltaX) e.preventDefault();
+    }
+  });
 
-    if (cardParent.mouseDrag) cardParent.mouseDrag.checkAutoScroll();
+  function paint() {
+    win.height = window.innerHeight;
+    win.width = window.innerWidth;
+    win.scrollX = window.scrollX;
+    win.scrollY = window.scrollY;
+
+    cardsWrapper.style.transform = `scale(${camera.scale}) translate(${-camera.x}px, ${-camera.y}px)`;
+    document.body.style.backgroundPosition = `${-(camera.x % GRID_SIZE) * camera.scale}px ${-(camera.y % GRID_SIZE) * camera.scale}px`;
+    document.body.style.backgroundSize = GRID_SIZE * camera.scale + 'px';
+
+    // if (cardParent.mouseDrag) cardParent.mouseDrag.checkAutoScroll();
 
     window.requestAnimationFrame(paint);
   }
